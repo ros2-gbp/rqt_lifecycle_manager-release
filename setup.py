3fd64c409836f0508ed53eeb022fb0aae2cbe920
Alberto Tudela <ajtudela@gmail.com>
2026-09-07 12:10:21 +0200

    Rebase from 'debian/jazzy/rqt_lifecycle_manager'

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ package_name = 'rqt_lifecycle_manager'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
